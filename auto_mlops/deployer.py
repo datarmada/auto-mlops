75b3fb5a6700a548203d07b147bb7069e340ae6f
Julien <ju.duquesne@hotmail.fr>
2020-02-19 15:15:55 +0100

    fix

--- auto_mlops/deployer.py
+++ auto_mlops/deployer.py
@@ -1,11 +1,15 @@
 import pickle
-import tempfile
-import requests 
+
+import requests
 
 class Deployer():
+    def __init__(self):
+        self.route = None
 
     def deploy(self, model):
-        with tempfile.TemporaryFile() as fp:
-            pickle.dump(model, fp)
-            res = requests.post("https://cloud.datarmada.com/upload", {"model":fp})
-        print(res)
+        with open("model,") as fp:
+            data = pickle.dump(model, fp)
+            res = requests.post("https://cloud.datarmada.com/upload", files={"model": data})
+        self.route = res.json()["route"]
+        print(f"Your model has been deployed to {self.route}")
+        return self.route
--- auto_mlops/utils.py
+++
@@ -1,5 +0,0 @@
-import datetime
-
-def get_current_normalized_date():
-    current_date = datetime.datetime.now().strftime("%m%d%Y%H%M%S")
-    return current_date
