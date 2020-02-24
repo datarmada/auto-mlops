1485b97f56609dadf7bf2caf34cd532ee2cb482b
Julien Duquesne <ju.duquesne@hotmail.fr>
2020-02-24 15:57:40 +0100

    raise Error if model is not sklearn

--- auto_mlops/deployer.py
+++ auto_mlops/deployer.py
@@ -1,15 +1,24 @@
+import logging
 import pickle
 
 import requests
+from sklearn.base import BaseEstimator
+
 
 class Deployer():
     def __init__(self):
         self.route = None
 
-    def deploy(self, model):
-        with open("model,") as fp:
+    def deploy(self, model: BaseEstimator):
+        if not issubclass(model.__class__, BaseEstimator):
+            raise TypeError("Your model must be a scikit-learn model")
+        with open("model") as fp:
             data = pickle.dump(model, fp)
             res = requests.post("https://cloud.datarmada.com/upload", files={"model": data})
-        self.route = res.json()["route"]
-        print(f"Your model has been deployed to {self.route}")
-        return self.route
+        if res.status_code == 200:
+            self.route = res.json()["route"]
+            print(f"Your model has been deployed to {self.route}")
+            return self.route
+        else:
+            logging.error(f"The request responded with the status code {res.status_code}")
+            logging.error(res.json())
