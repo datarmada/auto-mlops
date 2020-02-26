fbf017d1fff277a5fccdd9e49afa01ef87b199e1
Boris Ghidaglia <borisghidaglia@gmail.com>
2020-02-26 12:04:41 +0100

    Modified setup.py and pyproject.toml to try to fix package installation problems

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="auto-mlops-datarmada",
-    version="0.0.4",
+    version="0.1.7",
     author="Datarmada",
     author_email="contact@datarmada.com",
     description="Deploy your ML model with 1 line of Python",
@@ -17,6 +17,10 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
-    install_requires=["requests"]
+    python_requires='>=3.7',
+    install_requires=[
+        "scikit-learn = ^0.22.1",
+        "requests = ^2.23.0",
+        "cloudpickle = ^1.3.0",
+    ]
 )
